eb7f63952fb2927544735967ceb2f7d88dd19c7f
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-11-29 22:15:19 +0530

    setup backend for cleark auth

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
-
-import cors from "cors";
+import userRoutes from "./routes/user.route.js";
+import adminRoutes from "./routes/admin.route.js";
+import authRoutes from "./routes/auth.route.js";
+import songRoutes from "./routes/song.route.js";
+import albumRoutes from "./routes/album.route.js";
+import statRoutes from "./routes/stat.route.js";
+import { clerkMiddleware } from "@clerk/express";
 
 dotenv.config();
 
@@ -18,6 +24,14 @@ app.use(
 );
 
 app.use(express.json());
+app.use(clerkMiddleware());
+
+app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/songs", songRoutes);
+app.use("/api/albums", albumRoutes);
+app.use("/api/stats", statRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
