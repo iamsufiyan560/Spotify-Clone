a057b53d05b4538be2018ee4d389bb4902e5003c
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-11-30 21:57:30 +0530

    add Admin routes and controllers

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import fileUpload from "express-fileupload";
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
 import userRoutes from "./routes/user.route.js";
@@ -12,7 +14,7 @@ import statRoutes from "./routes/stat.route.js";
 import { clerkMiddleware } from "@clerk/express";
 
 dotenv.config();
-
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT;
 
@@ -26,12 +28,32 @@ app.use(
 app.use(express.json());
 app.use(clerkMiddleware());
 
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+      fileSize: 10 * 1024 * 1024,
+    },
+  })
+);
+
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message:
+      process.env.NODE_ENV === "production"
+        ? "Internal server error"
+        : err.message,
+  });
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
