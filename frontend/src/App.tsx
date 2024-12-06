f570da65948ab53c2f3d71a2fa32686510becf1d
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-12-06 21:45:02 +0530

    seed songs and albums

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route
+          path="/sso-callback"
+          element={
+            <AuthenticateWithRedirectCallback
+              signUpForceRedirectUrl={"/auth-callback"}
+            />
+          }
+        />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
