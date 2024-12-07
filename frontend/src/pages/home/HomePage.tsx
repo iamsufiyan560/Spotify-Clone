3ef8de2f817c9a2eb4cfefab8419dfc5d20b1287
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-12-07 15:30:00 +0530

    add feature songs

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,11 +1,28 @@
 import Topbar from "@/components/Topbar";
+import FeaturedSection from "./components/FeaturedSection";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useMusicStore } from "@/stores/useMusicStore";
+import { useEffect } from "react";
 
 const HomePage = () => {
+  const { fetchFeaturedSongs } = useMusicStore();
+
+  useEffect(() => {
+    fetchFeaturedSongs();
+  }, [fetchFeaturedSongs]);
+
   return (
     <main className="rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900">
       <Topbar />
+      <ScrollArea className="h-[calc(100vh-180px)]">
+        <div className="p-4 sm:p-6">
+          <h1 className="text-2xl sm:text-3xl font-bold mb-6">
+            Good afternoon
+          </h1>
+          <FeaturedSection />
+        </div>
+      </ScrollArea>
     </main>
   );
 };
-
 export default HomePage;
